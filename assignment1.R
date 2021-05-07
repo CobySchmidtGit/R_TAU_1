4446625cc8c3f42bf1d0f296547cbe778b17a5fb
CobySchmidtGit <cobys.schmidt@gmail.com>
2021-05-07 13:27:33 +0300

    Dog garden VS Food Shops

--- assignment1.R
+++ assignment1.R
@@ -4,3 +4,5 @@ assign1.csv<-read.csv("~/Downloads/assign1.csv")
 #Plotting a Histogram with 20 columns/bins
 hist(assign1.csv$y, breaks=20)
 
+#plotting a X->Y graph where X is number of Food Shops and Y is the number of Dog Gardens
+plot(assign1.csv$X5,assign1.csv$X4)
