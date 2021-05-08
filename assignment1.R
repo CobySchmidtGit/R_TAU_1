d39742828a6f19acf4282acb3ded3be633a806fb
CobySchmidtGit <cobys.schmidt@gmail.com>
2021-05-08 23:40:05 +0300

    Update assignment1.R

--- assignment1.R
+++ assignment1.R
@@ -1,7 +1,7 @@
 #Reading the input CSV file of Apartment prices
-assign1.csv<-read.csv("~/Downloads/assign1.csv")
+assign1.csv<-read.csv("assign1.csv")
 
-#Plotting a Histogram with 20 columns/bins
+ger#Plotting a Histogram with 20 columns/bins
 hist(assign1.csv$y, breaks=20)
 
 #plotting a X->Y graph where X is number of Food Shops and Y is the number of Dog Gardens
